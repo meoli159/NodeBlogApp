d53f7cb19159dfd35a78cac7a6aa2e1c9cfe5fb4
Cyrus <kur0dk0@gmail.com>
2023-11-30 02:33:32 +0700

    Add Create Post

--- BE/src/controllers/v1/post.js
+++ BE/src/controllers/v1/post.js
@@ -1,18 +1,40 @@
 import { Post } from '../../models/post.js';
 import '../../models/user.js';
 import '../../models/comment.js';
+import { User } from '../../models/user.js';
 export const getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find().populate('owner', 'name created_at');
 
     if (!posts.length > 0) return res.json({ message: 'There is no post!!' });
-    return res.json(posts);
+    return res.status(200).json(posts);
   } catch (error) {
     console.error(error);
     res.status(400).send('Bad Request');
   }
 };
 
-export const createPost = () => {};
-export const updatePost = () => {};
-export const deletePost = () => {};
+export const createPost = async (req, res) => {
+  try {
+    const { ownerId, title, content, tags } = req.body;
+    const user = await User.findById(ownerId);
+    if (!user) return res.status(404).send('Please Login');
+    const post = await Post.create({
+      owner: user._id,
+      title: title,
+      content: content,
+      tags: tags,
+    });
+    return res.status(200).json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('Bad Request');
+  }
+};
+export const updatePost = async (req, res) => {
+  const { owner, title, content, tag } = req.body;
+  const { id } = req.params;
+};
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+};
--- BE/src/routes/v1/post.js
+++ BE/src/routes/v1/post.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getAllPosts, createPost } from '../../controllers/v1/post.js';
+import {
+  getAllPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../../controllers/v1/post.js';
 const postRoutes = express.Router();
 
 postRoutes.get('/', getAllPosts);
 postRoutes.post('/', createPost);
-
+postRoutes.patch('/:id', updatePost);
+postRoutes.delete('/:id', deletePost);
 export { postRoutes };
